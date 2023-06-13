d5af012d6a7d01a6ce34abe19343123fa9a1ca3b
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-06-13 16:23:56 +0000

    added splashScene

---
+++ js/splashScene.js
@@ -0,0 +1,69 @@
+/* global Phaser */
+
+// Copyright (c) 2023   Lucas Osmond All rights reserved
+//
+// Created by: Lucas Osmond
+// Created on: April 2023
+//This is the Splash Scene
+
+/**
+ * This class is the splash scene.
+ */
+class SplashScene extends Phaser.Scene {
+  /**
+   * This method is the constructor.
+   */
+  constructor() {
+    super({ key: "SplashScene" });
+
+    this.splashSceneBackroundImage = null;
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called when the Scene is started by the SceneManager.
+   *before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  init() {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets
+   */
+  preload() {
+    console.log("Splash Scene");
+    this.load.image("splashSceneBackroundImage", "./assets/splashScene.png");
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  create(data) {
+    this.splashSceneBackroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackroundImage"
+    );
+    this.splashSceneBackroundImage.x = 1920 / 2;
+    this.splashSceneBackroundImage.y = 1080 / 2;
+  }
+
+  /**
+   * Should be overridden on your own Scenes.
+   * This method is called once per game step while the scene is running.
+   * @param {number} time - The current time.
+   * @param {number} delta - The delta time in ms since the last frame.
+   */
+  update(time, delta) {
+    if (time > 3000) {
+      this.scene.switch("TitleScene");
+    }
+  }
+}
+
+export default SplashScene;
